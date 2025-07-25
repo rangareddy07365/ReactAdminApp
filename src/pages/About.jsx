cff2ae369cb87725f8d939f9bccac7e62c5d95c0
rangareddy07365 <nrangareddy07@gmail.com>
2025-07-25 18:49:58 -0500

    Updated About and Contact Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../assets/Header.css";
+import "../assets/AdminLayout.css";
 
 const About = () => {
-    return(
-        <div>
-            About
-        </div>
-    );
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Ranga Reddy Nandyala</p>
+      <p>Software Developer</p>
+      <p>OTSI</p>
+      <Link to={"/"}>
+        <button className="themeButton">Home</button>
+      </Link>
+    </div>
+  );
 };
 export default About;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../assets/Header.css";
+import "../assets/AdminLayout.css";
 
 const Contact = () => {
-    return(
-        <div>
-            Contact
-        </div>
-    );
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>Ranga Reddy Nandyala</p>
+      <p>Ph No: 4694961737</p>
+      <p>email: nrangareddy07@gmail.com</p>
+      <Link to={"/"}>
+        <button className="themeButton">Home</button>
+      </Link>
+    </div>
+  );
 };
 
 export default Contact;
